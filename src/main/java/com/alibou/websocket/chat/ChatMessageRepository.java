92c0dd20036d86d667650730eb91e1a95369220c
Wunmi <atiwunmi@yahoo.com>
2024-03-17 20:27:19 -0600

    m

--- src/main/java/com/alibou/websocket/chat/ChatMessageRepository.java
+++
@@ -1,9 +0,0 @@
-package com.alibou.websocket.chat;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-import java.util.List;
-
-public interface ChatMessageRepository extends MongoRepository<ChatMessage, String> {
-    List<ChatMessage> findByChatId(String chatId);
-}
